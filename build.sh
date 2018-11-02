be7666608c2b91311714271da22cca61592c4935
lvlts <none>
2018-11-02 17:03:12 +0100

    miredo tests; suppress config

--- build.sh
+++ build.sh
@@ -3,6 +3,9 @@ die() { echo "$*" 1>&2 ; exit 1; }
 
 node --version
 
+sleep 40
+ip addr
+
 ## pull latest commits of cjdns and hyperboria-peers
 
 git submodule update --recursive --remote
@@ -13,17 +16,19 @@ popd
 
 echo "[+] generating default cjdns configuration"
 echo -n "module.exports = " > cjdroute.conf
-cjdns/cjdroute --genconf | tee -a cjdroute.conf
+cjdns/cjdroute --genconf | tee -a cjdroute.conf > /dev/null
 
 echo "[+] adding current peers (hyperboria/peers) to current configuration"
-./main | tee cjdroute.populated.conf
+./main | tee cjdroute.populated.conf > /dev/null
 
 echo "[+] starting cjdns"
 cjdns/cjdroute < cjdroute.populated.conf || die "Unable to start cjdns"
 
 echo "[+] waiting for connection (180s)"
 sleep 180
-
+ps fauxww
+ip addr
+cat /etc/miredo.conf
 mkdir -p out
 cp .page_template.md out/index.md
 ./peerStats | tee -a out/index.md
